fdcfe4661017d2977b90a4d40e06649a4855993a
ilovegithub15 <142916985+ilovegithub15@users.noreply.github.com>
2024-10-13 23:37:34 -0500

    added final functions

--- matrixmanipulation.c
+++ matrixmanipulation.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #define SIZE 5
 void addMatrices(int a[SIZE][SIZE], int b[SIZE][SIZE], int result[SIZE][SIZE]) {
+    //adds each term
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             result[i][j] = a[i][j] + b[i][j];
@@ -8,11 +9,31 @@ void addMatrices(int a[SIZE][SIZE], int b[SIZE][SIZE], int result[SIZE][SIZE]) {
     }
 }
 void printMatrix(int matrix[SIZE][SIZE]) {
+    //prints the matrix
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
+    }
+}
+void multiplyMatrices(int a[SIZE][SIZE], int b[SIZE][SIZE], int result[SIZE][SIZE]) {
+    //simply multiplies each respective term
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < SIZE; k++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+void transposeMatrix(int a[SIZE][SIZE], int result[SIZE][SIZE]) {
+    //switches positions of each of the terms
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result[j][i] = a[i][j];
+        }
     }
 }
 int main() {
@@ -32,5 +53,7 @@ int main() {
     };
     int result[SIZE][SIZE];
     addMatrices(m1, m2, result);
+    printMatrix(result);
+    multiplyMatrices(m1,m2,result);
     printMatrix(result);
 }
