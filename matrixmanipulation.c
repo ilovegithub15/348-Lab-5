37ebcad0b8f608385e56723225603579fe70ef81
ilovegithub15 <142916985+ilovegithub15@users.noreply.github.com>
2024-10-13 23:24:09 -0500

    added second function and tested

--- matrixmanipulation.c
+++ matrixmanipulation.c
@@ -7,3 +7,30 @@ void addMatrices(int a[SIZE][SIZE], int b[SIZE][SIZE], int result[SIZE][SIZE]) {
         }
     }
 }
+void printMatrix(int matrix[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int m1[SIZE][SIZE] = {
+    {1, 2, 3, 4, 5},
+    {6, 7, 8, 9, 10},
+    {11, 12, 13, 14, 15},
+    {16, 17, 18, 19, 20},
+    {21, 22, 23, 24, 25}
+    };
+    int m2[SIZE][SIZE] = {
+    {25, 24, 23, 22, 21},
+    {20, 19, 18, 17, 16},
+    {15, 14, 13, 12, 11},
+    {10, 9, 8, 7, 6},
+    {5, 4, 3, 2, 1}
+    };
+    int result[SIZE][SIZE];
+    addMatrices(m1, m2, result);
+    printMatrix(result);
+}
