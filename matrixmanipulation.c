45e49e254cfb3a3f47d7ddbc6218ba96b3bd35d6
ilovegithub15 <142916985+ilovegithub15@users.noreply.github.com>
2024-10-13 23:18:49 -0500

    added addMatricies function

---
+++ matrixmanipulation.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define SIZE 5
+void addMatrices(int a[SIZE][SIZE], int b[SIZE][SIZE], int result[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
